3dc7f5990b197bc12d641309ad9c1ef5fe953d8a
Alexandru Pisica <alexandru.pisica@bluepoint-it.ro>
2023-05-02 14:45:35 +0300

    alex_125

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,10 +1,15 @@
+import { db } from 'src/lib/db'
+
 import { comments, createComment } from './comments'
 
-describe('comments', () => {
-  scenario('returns all comments', async (scenario) => {
-    const result = await comments()
-
-    expect(result.length).toEqual(Object.keys(scenario.comment).length)
+scenario(
+  'returns all comments for a single post from the database',
+  async (scenario) => {
+    const result = await comments({ postId: scenario.comment.jane.postId })
+    const post = await db.post.findUnique({
+      where: { id: scenario.comment.jane.postId },
+      include: { comments: true },
+    })
   })
 
   scenario('postOnly', 'creates a new comment', async (scenario) => {
@@ -22,5 +27,10 @@ describe('comments', () => {
     expect(comment.body).toEqual('What is your favorite tree bark?')
     expect(comment.postId).toEqual(scenario.post.bark.id)
     expect(comment.createdAt).not.toEqual(null)
+  })
+
+  scenario('returns all comments', async (scenario) => {
+    const result = await comments({ postId: scenario.comment.jane.postId })
+    expect(result.length).toEqual(Object.keys(scenario.comment).length)
   })
 })
